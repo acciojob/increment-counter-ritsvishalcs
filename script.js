ea27a81961d7f5db1a637e45c6915907a8b65471
Vishal Kumar <98923664+ritsvishalcs@users.noreply.github.com>
2025-05-19 22:34:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+const counterElem = document.getElementById('counter');
+const btn = document.getElementById('incrementBtn');
+
+btn.addEventListener('click', () => {
+  // Get the current count as a number
+  const currentCount = Number(counterElem.textContent);
+
+  // Show alert with current un-incremented value
+  alert(currentCount);
+
+  // Increment counter and update paragraph
+  counterElem.textContent = currentCount + 1;
+});
